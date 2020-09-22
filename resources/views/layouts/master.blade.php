db23aa5e5245de70a08fadc9e23ae0725385ecaa
Davis Too <33203305+kipkiruitoo@users.noreply.github.com>
2020-09-22 12:09:29 +0300

    added our logo and set up everything

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Daybyday CRM</title>
+    <title>Exergy CRM</title>
     <link href="{{ URL::asset('css/jasny-bootstrap.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ URL::asset('css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ URL::asset('css/dropzone.css') }}" rel="stylesheet" type="text/css">
@@ -37,7 +37,7 @@
 
     <nav id="myNavmenu" class="navmenu navmenu-default navmenu-fixed-left offcanvas-sm" role="navigation">
         <div class="list-group panel">
-            <p class=" list-group-item siderbar-top" title=""><img src="{{url('images/daybyday-logo-white.png')}}" alt="" style="width: 100%; margin: 1em 0;"></p>
+            <p class=" list-group-item siderbar-top" title=""><img  height="100" width="100"  src="{{url('images/daybyday-logo-white.png')}}" alt="" style=" margin: 1em 0;"></p>
             <a href="{{route('dashboard')}}" class=" list-group-item" data-parent="#MainMenu"><i
                         class="fa fa-home sidebar-icon"></i><span id="menu-txt">{{ __('Dashboard') }} </span></a>
             <a href="{{route('users.show', \Auth::user()->external_id)}}" class=" list-group-item"
